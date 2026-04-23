2ec3ea688b22168693c47666b82fe4f10021b38e
fedddot <idanrommm@gmail.com>
2026-04-23 07:24:12 +0000

    refactor: reorganize includes and add unit test for FrameWriter constructor

--- frame/tests/src/ut_frame.cpp
+++ frame/tests/src/ut_frame.cpp
@@ -1,7 +1,8 @@
+#include "gtest/gtest.h"
+
 #include "frame_reader.hpp"
 #include "frame_writer.hpp"
 #include "ring_buffer.hpp"
-#include "gtest/gtest.h"
 
 using namespace nanoipc;
 
@@ -11,4 +12,14 @@ TEST(ut_frame_reader, ctor) {
 
     // THEN
     ASSERT_NO_THROW(FrameReader reader(&ring_buffer));
+}
+
+TEST(ut_frame_writer, ctor) {
+    // WHEN
+    const auto dummy_raw_data_writer = [](const std::uint8_t *raw_data, const std::size_t raw_data_size) {
+
+    };
+
+    // THEN
+    ASSERT_NO_THROW(FrameWriter writer(dummy_raw_data_writer));
 }

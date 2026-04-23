eaaebec2dc4158f0ba9d93140fc2570bd3ab45e6
fedddot <idanrommm@gmail.com>
2026-04-23 14:49:41 +0000

    feat: refactor json_client to use serialib for UART communication and enhance JSON message handling

--- frame/linux/src/json_client.cpp
+++ frame/linux/src/json_client.cpp
@@ -1,49 +1,75 @@
+#include <cstddef>
+
 #include "linux_uart_cobs_frame_reader.hpp"
 #include "linux_uart_cobs_frame_writer.hpp"
 #include "json_message_reader.hpp"
 #include "json_message_writer.hpp"
-#include "ring_buffer.hpp"
+#include "serialib.h"
 #include "json/value.h"
 
 using namespace nanoipc;
 
-int main() {
-    RingBuffer<512> ring_buffer;
-    UartConnection uart(
-        "/dev/ttyACM0",
-        9600,
-        SERIAL_PARITY_NONE,
-        SERIAL_STOPBITS_1,
-        SERIAL_DATABITS_8,
-        [&ring_buffer](uint8_t c) {
-            ring_buffer.push_back(c);
-        }
+static unsigned int get_baud(int argc, char* argv[]);
+static SerialParity get_parity(int argc, char* argv[]);
+static SerialDataBits get_data_bits(int argc, char* argv[]);
+static SerialStopBits get_stop_bits(int argc, char* argv[]);
+static std::string get_serial_port(int argc, char* argv[]);
+static Json::Value read_json_message(int argc, char* argv[]);
+
+enum: std::size_t { BUFF_SIZE = 1024UL };
+
+int main(int argc, char* argv[]) {
+    serialib uart;
+    const auto open_result = uart.openDevice(
+        get_serial_port(argc, argv).c_str(),
+        get_baud(argc, argv),
+        get_data_bits(argc, argv),
+        get_parity(argc, argv),
+        get_stop_bits(argc, argv)
     );
-    CobsFrameReader cobs_reader(&ring_buffer);
-    CobsFrameWriter cobs_writer(
-        [&uart](const uint8_t* data, size_t length) {
-            uart.write(data, length);
-        }
-    );
+    if (open_result != 1) {
+        std::cerr << "Failed to open serial port: " << get_serial_port(argc, argv) << std::endl;
+        return 1;
+    }
+    LinuxUartCobsFrameReader<BUFF_SIZE> cobs_reader(&uart);
+    LinuxUartCobsFrameWriter cobs_writer(&uart);
     JsonMessageReader json_reader(&cobs_reader);
     JsonMessageWriter json_writer(&cobs_writer);
 
-    uart.open();
-
-    Json::Value message;
-    message["type"] = "greeting";
-    message["content"] = "Hello from the JSON client!";
-    json_writer.write(message);
+    const auto request = read_json_message(argc, argv);
+    json_writer.write(request);
     while (true) {
-        const auto received_message = json_reader.read();
-        if (!received_message.has_value()) {
+        const auto response = json_reader.read();
+        if (!response.has_value()) {
             continue;
         }
-        const auto& msg = received_message.value();
-        std::cout << "Received message: " << msg.toStyledString() << std::endl;
+        const auto msg = response.value();
+        std::cout << "Received response: " << msg.toStyledString() << std::endl;
         break;
     }
-    uart.close();
+    uart.closeDevice();
 
    return 0;
+}
+
+unsigned int get_baud(int argc, char* argv[]) {
+    return 9600; // TODO: retrieve from command line arguments (--baud)
+}
+SerialParity get_parity(int argc, char* argv[]) {
+    return SerialParity::SERIAL_PARITY_NONE; // TODO: retrieve from command line arguments (--parity)
+}
+SerialDataBits get_data_bits(int argc, char* argv[]) {
+    return SerialDataBits::SERIAL_DATABITS_8; // TODO: retrieve from command line arguments (--data-bits)
+}
+SerialStopBits get_stop_bits(int argc, char* argv[]) {
+    return SerialStopBits::SERIAL_STOPBITS_1; // TODO: retrieve from command line arguments (--stop-bits)
+}
+std::string get_serial_port(int argc, char* argv[]) {
+    return "/dev/ttyACM0"; // TODO: retrieve from command line arguments (--port)
+}
+Json::Value read_json_message(int argc, char* argv[]) {
+    Json::Value request;
+    request["type"] = "test_request";
+    request["payload"] = "Hello, world!";
+    return request; // TODO: retrieve from command line arguments (--request-file)
 }

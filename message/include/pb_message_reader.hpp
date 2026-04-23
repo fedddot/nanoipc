d6897088a89dee8b7bff924161a1fead1a9ff4cf
fedddot <idanrommm@gmail.com>
2026-04-23 07:56:53 +0000

    feat: add message handling with nanopb support and unit tests

---
+++ message/include/pb_message_reader.hpp
@@ -0,0 +1,68 @@
+#ifndef	PB_MESSAGE_READER_HPP
+#define	PB_MESSAGE_READER_HPP
+
+#include <cstddef>
+#include <cstdint>
+#include <optional>
+#include <stdexcept>
+#include <vector>
+
+#include "read_buffer.hpp"
+#include "reader.hpp"
+#include "cobs.h"
+
+namespace nanoipc {
+	class PbMessageReader: public Reader<std::vector<std::uint8_t>> {
+	public:
+		PbMessageReader(ReadBuffer *read_buffer): m_read_buffer(read_buffer) {
+			if (!m_read_buffer) {
+				throw std::invalid_argument("read_buffer cannot be null");
+			}
+		}
+		PbMessageReader(const PbMessageReader&) = default;
+		PbMessageReader& operator=(const PbMessageReader&) = default;
+
+		std::optional<std::vector<std::uint8_t>> read() override {
+            const auto encoded_frame = read_encoded_frame(m_read_buffer);
+            if (!encoded_frame.has_value()) {
+                return std::nullopt;
+            }
+            return std::make_optional(decode_frame(encoded_frame.value()));
+		}
+
+	private:
+		ReadBuffer *m_read_buffer;
+		static std::optional<std::size_t> find_delimiter(const ReadBuffer& read_buffer) {
+			for (auto i = std::size_t(0); i < read_buffer.size(); ++i) {
+				if (read_buffer.get(i) == COBS_PB_MESSAGE_DELIMITER) {
+					return i;
+				}
+			}
+			return std::nullopt;
+		}
+        static std::optional<std::vector<std::uint8_t>> read_encoded_frame(ReadBuffer *read_buffer) {
+            const auto delimiter_index = find_delimiter(*read_buffer);
+			if (!delimiter_index.has_value()) {
+				return std::nullopt;
+			}
+			std::vector<std::uint8_t> encoded_frame_data;
+			encoded_frame_data.reserve(delimiter_index.value() + 1);
+			for (auto i = std::size_t(0); i <= delimiter_index.value(); ++i) {
+				encoded_frame_data.push_back(read_buffer->pop_front());
+			}
+			return std::make_optional(encoded_frame_data);
+        }
+        static std::vector<std::uint8_t> decode_frame(const std::vector<std::uint8_t>& frame_data) {
+            std::vector<std::uint8_t> decoded_frame_data(frame_data.size());
+            std::size_t decoded_frame_data_size = 0;
+            const auto decode_result = cobs_decode(frame_data.data(), frame_data.size(), decoded_frame_data.data(), decoded_frame_data.size(), &decoded_frame_data_size);
+            if (decode_result != COBS_RET_SUCCESS) {
+                throw std::runtime_error("failed to decode COBS frame");
+            }
+            decoded_frame_data.resize(decoded_frame_data_size);
+            return decoded_frame_data;
+        }
+	};
+}
+
+#endif // PB_MESSAGE_READER_HPP
---
+++ message/include/pb_message_writer.hpp
@@ -0,0 +1,55 @@
+#ifndef	PB_MESSAGE_WRITER_HPP
+#define	PB_MESSAGE_WRITER_HPP
+
+#include <cstddef>
+#include <cstdint>
+#include <functional>
+#include <stdexcept>
+#include <vector>
+
+#include "writer.hpp"
+#include "cobs.h"
+
+namespace nanoipc {
+	class FrameWriter: public Writer<std::vector<std::uint8_t>> {
+	public:
+		using RawDataWriter = std::function<void(const std::uint8_t *raw_data, const std::size_t raw_data_size)>;
+		FrameWriter(const RawDataWriter& raw_data_writer): m_raw_data_writer(raw_data_writer) {
+			if (!m_raw_data_writer) {
+				throw std::invalid_argument("raw_data_writer cannot be empty");
+			}
+		}
+		FrameWriter(const FrameWriter&) = default;
+		FrameWriter& operator=(const FrameWriter&) = default;
+
+		void write(const std::vector<std::uint8_t>& data) const override {
+			const auto encoded_message = encode_frame(data);
+			m_raw_data_writer(encoded_message.data(), encoded_message.size());
+		}
+
+	private:
+		RawDataWriter m_raw_data_writer;
+        static std::vector<std::uint8_t> encode_frame(const std::vector<std::uint8_t>& data) {
+            enum: std::size_t { SIZE_INCREMENT = 2 };
+            std::vector<std::uint8_t> encoded_frame_data;
+            std::size_t receiver_data_size = data.size() + SIZE_INCREMENT;
+            std::size_t encoded_frame_data_size = 0;
+
+            while (true) {
+                encoded_frame_data.resize(receiver_data_size);
+                const auto encode_result = cobs_encode(data.data(), data.size(), encoded_frame_data.data(), encoded_frame_data.size(), &encoded_frame_data_size);
+                if (encode_result == COBS_RET_SUCCESS) {
+                    break;
+                } else if (encode_result == COBS_RET_ERR_EXHAUSTED) {
+                    receiver_data_size += SIZE_INCREMENT;
+                } else {
+                    throw std::runtime_error("failed to encode COBS frame");
+                }
+            }
+            encoded_frame_data.resize(encoded_frame_data_size);
+            return encoded_frame_data;
+        }
+	};
+}
+
+#endif // PB_MESSAGE_WRITER_HPP
